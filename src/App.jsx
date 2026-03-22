5d69f81c0d8a92e407f585f6f496a0be54d8bf84
Vox <wbdev.vox@gmail.com>
2026-03-22 11:11:36 +0400

    feat: интеграция с API - Добавление задачи

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
   useEffect(() => {
     axios.get("http://127.0.0.1:8000/api/tasks").then((response) => {
@@ -10,17 +11,47 @@ function App() {
     });
   }, []);
 
+  const addTask = (e) => {
+    // Блокируем перезагрузку страницы
+    e.preventDefault();
+
+    // Отправляем POST-запрос
+    axios
+      .post("http://127.0.0.1:8000/api/tasks", {
+        title: newTask, // Отправляем на бэкенд наш текст из стейта
+      })
+      .then((response) => {
+        // Очищаем инпут (чтобы можно было вводить новую задачу)
+        setNewTask("");
+
+        // Обновляем список задач на экране, добавляя новую задачу в конец массива
+        setTasks([...tasks, response.data.data]);
+      })
+      .catch((error) => {
+        console.error("Ошибка при добавлении:", error);
+      });
+  };
+
   return (
     <div>
       <h1>Мои задачи на Laravel</h1>
 
-      <ul>
-        {tasks.map((task) => (
+      <form onSubmit={addTask}>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button>Добавить</button>
+      </form>
+
+      {tasks.map((task) => (
+        <ul>
           <li key={task.id}>
-            {task.title} - {task.description}
+            {task.id}. {task.title} - {task.description}
           </li>
-        ))}
-      </ul>
+        </ul>
+      ))}
     </div>
   );
 }
